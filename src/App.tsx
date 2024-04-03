149cdb243c59601dc81d1f698f64bd94e4fbd59b
Faleye Benjamin <bofaleye@hotmail.com>
2024-04-03 10:07:43 +0100

    format test

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import "./App.css";
 
 function App() {
   return (
- <div className="App">
-         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+<div className="App">
+            <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to copy.
         </p>
