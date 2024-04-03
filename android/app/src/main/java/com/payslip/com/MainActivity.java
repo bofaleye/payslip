2c9f922675a3dac0a6d7fb0f888ae33c9141137c
Faleye Benjamin <bofaleye@hotmail.com>
2024-04-03 09:31:41 +0100

    all settings done

---
+++ android/app/src/main/java/com/payslip/com/MainActivity.java
@@ -0,0 +1,5 @@
+package com.payslip.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.payslip.com',
+  appName: 'payslip',
+  webDir: 'build',
+  server: {
+     androidScheme: 'https',
+    // "url": "http://192.168.1.2:8100",
+  // "cleartext": true
+  }
+};
+
+export default config;
